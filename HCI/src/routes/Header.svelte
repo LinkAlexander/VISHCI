25a84f5ad97f818771f5ba19d8a101f0f06ae092
RebornStreicherJo <jonatan.kunze@gmx.net>
2024-05-23 16:03:18 +0200

    get data with sql queries and postgresql js

--- HCI/src/routes/Header.svelte
+++ HCI/src/routes/Header.svelte
@@ -28,6 +28,9 @@
 			<li aria-current={$page.url.pathname === '/d3demo' ? 'page' : undefined}>
 				<a href="/d3demo">D3 Demo</a>
 			</li>
+			<li aria-current={$page.url.pathname === '/d3withdata' ? 'page' : undefined}>
+				<a href="/d3withdata">D3 with Data</a>
+			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
---
+++ HCI/src/routes/d3withdata/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+  //import * as d3 from "d3";
+  export let data;
+  console.log(data);
+  console.log("-----------");
+  console.assert(data.result[0]);
+</script>
+
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<div>
+<h1>Hello World</h1>
+<h5>{data.result[0]}</h5>
+</div>
