f0761d9088dca8c9d31f6eb7124052904003a580
Alexander Link <alexanderlink277@gmail.com>
2024-05-26 09:44:34 +0200

    rgbValues for Countries. TODO Lookup

--- HCI/src/routes/d3Globe/+page.svelte
+++ HCI/src/routes/d3Globe/+page.svelte
@@ -2,10 +2,16 @@
     import { onMount } from "svelte";
     import * as d3 from "d3";
     import worldData from "./world-110m.json";
+    // import refData from "./data.json";
     import * as topojson from "topojson-client"; // Du brauchst die GeoJSON-Daten der Weltkarte
+    import { ColorConverter } from "three/examples/jsm/Addons.js";
 
+    /**
+     * @type {SVGSVGElement}
+     */
     let svg;
     let countries;
+    // let countryLookup;
 
     onMount(async () => {
         const width = 960;
@@ -21,6 +27,7 @@
             .attr("width", width)
             .attr("height", height);
 
+        // @ts-ignore
         countries = topojson.feature(worldData, worldData.objects.countries).features;
 
         svgElement.selectAll("path")
@@ -31,13 +38,25 @@
             .style("fill", d => getCountryColor(d.id)); // getCountryColor ist eine Funktion zur Bestimmung der Farbe
 
         // Beispiel-Funktion zur Farbgebung basierend auf der Länder-ID
+        /**
+         * @param {number} countryId
+         */
         function getCountryColor(countryId) {
             // Hier kannst du deine Logik zur Farbgebung einfügen
             const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
-            if(countryId == 840) return "red"; // USA
-            if(countryId == 276) return "blue"; // Deutschland
-
-            return "pink";
+            if(countryId == 840) return "rgb(255,0,0,255)"; // USA
+            if(countryId == 276) return "rgb(0,0,255,255)"; // Deutschland
+            if(countryId == findCountryIDbyName("Canada")) return "rgb(0,255,255,255)";
+            
+            return "rgb(0,0,0,255)";
+        }
+        // Beispiel-Funktion zur Farbgebung basierend auf der Länder-ID
+        /**
+         * @param {string} name
+         * @returns {number}
+         */
+        function findCountryIDbyName(name) {
+            return 124;
         }
     });
 </script>
